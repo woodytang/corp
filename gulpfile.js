8987af501003c817441278e66b7181343daf9676
woodytang <tangsi@outlook.com>
2016-10-24 18:13:29 +0800

    icon-list

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 'use strict';
- 
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
- 
+
 gulp.task('sass', function () {
   return gulp.src('./src/sass/**/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(gulp.dest('./assets/css'));
 });
- 
+
 gulp.task('sass:watch', function () {
   gulp.watch('./src/sass/**/*.scss', ['sass']);
 });
@@ -18,13 +18,12 @@ var htmlImport = require('gulp-html-import');
 gulp.task('import', function () {
     gulp.src('./src/html/index.html')
         .pipe(htmlImport('./src/html/components/'))
-        .pipe(gulp.dest('./')); 
+        .pipe(gulp.dest('./'));
 })
 
 gulp.task('import:watch', function () {
-  gulp.watch('./src/html/index.html', ['import']);
+  gulp.watch('./src/html/**/*.html', ['import']);
 });
 
 
 gulp.task('default', ['sass:watch', 'import:watch']);
-
